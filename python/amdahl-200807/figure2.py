90fb8e3495b51e24e665b386aa8de29f64ccbf96
dthome9180 <dthome9180@gmail.com>
2017-04-05 14:29:42 +0800

    Remove all ax.annotate()
    Change some text style to 80 char width

--- python/amdahl-200807/figure2.py
+++ python/amdahl-200807/figure2.py
@@ -27,15 +27,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def SpeedupSymmetric(f,n,r):
-    s = 1.0*Perf(r)/((1-f)+(f*r)/n)
+    s = 1.0/(((1-f)/Perf(r))+(f/(n/r)))
     return s
     
 def SpeedupAsymmetric(f,n,r):
-    s = 1.0*Perf(r)/((1-f)+(f*Perf(r))/(Perf(r)+n-r))
+    s = 1.0/(((1-f)/Perf(r))+f/(Perf(r)+n-r))
     return s
     
 def SpeedupDynamic(f,n,r):
-    s = 1.0*Perf(r)/((1-f)+f*Perf(r)/n)
+    s = 1.0/(((1-f)/Perf(r))+f/n)
     return s
 
 def Perf(r):
@@ -46,144 +46,138 @@ def Perf(r):
 f = [0.999, 0.99, 0.975, 0.9, 0.5]
 
 fig, axs = plt.subplots(3,2)
-fig.subplots_adjust(left=0.08, right=0.98, wspace=0.3)
+#fig.subplots_adjust(left=0.08, right=0.98, wspace=0.3)
 
 # rBCEs, the number of total BCEs
 x = np.linspace(1, 16, num=16, endpoint=True)
 #SpeedupSymmetric N=16
 ax = axs[0,0]
-line16f0, = ax.plot(x, SpeedupSymmetric(f[0], 16, x), color='red', linewidth=2.0,
-                    linestyle="-", label='f=0.999')
-line16f1, = ax.plot(x, SpeedupSymmetric(f[1], 16, x), color='coral', linewidth=2.0,
-                    linestyle=":", label='f=0.99')
-line16f2, = ax.plot(x, SpeedupSymmetric(f[2], 16, x), color='green', linewidth=2.0,
-                    linestyle="-.", label='f=0.975')
-line16f3, = ax.plot(x, SpeedupSymmetric(f[3], 16, x), color='purple', linewidth=2.0,
-                    linestyle="--", label='f=0.9')
-line16f4, = ax.plot(x, SpeedupSymmetric(f[4], 16, x), color='blue', linewidth=2.0,
-                    linestyle="-", label='f=0.5')
+line16f0, = ax.plot(x, SpeedupSymmetric(f[0], 16, x), color='red', 
+                    linewidth=2.0,linestyle="-", label='f=0.999')
+line16f1, = ax.plot(x, SpeedupSymmetric(f[1], 16, x), color='coral', 
+                    linewidth=2.0,linestyle=":", label='f=0.99')
+line16f2, = ax.plot(x, SpeedupSymmetric(f[2], 16, x), color='green', 
+                    linewidth=2.0,linestyle="-.", label='f=0.975')
+line16f3, = ax.plot(x, SpeedupSymmetric(f[3], 16, x), color='purple', 
+                    linewidth=2.0,linestyle="--", label='f=0.9')
+line16f4, = ax.plot(x, SpeedupSymmetric(f[4], 16, x), color='blue', 
+                    linewidth=2.0,linestyle="-", label='f=0.5')
 
-#ax.legend(loc='upper right')
+#ax.legend(loc='upper right',fontsize='small')
 ax.set_xscale('log', basex=2)
 ax.set_xlim(1, 16)
 ax.set_ylim(2, 16)
 ax.set_xticks([1,2,4,8,16])
 ax.set_xticklabels(['0','2','4','8','16'])
-#ax.annotate('Symmetric, n=16', xy=(1.5, 15))
 
 # rBCEs, the number of total BCEs
 x = np.linspace(1, 256, num=256, endpoint=True)
 #SpeedupSymmetric N=256
 ax = axs[0,1]
-line256f0, = ax.plot(x, SpeedupSymmetric(f[0], 256, x), color='red', linewidth=2.0,
-                    linestyle="-", label='f=0.999')
-line256f1, = ax.plot(x, SpeedupSymmetric(f[1], 256, x), color='coral', linewidth=2.0,
-                    linestyle=":", label='f=0.99')
-line256f2, = ax.plot(x, SpeedupSymmetric(f[2], 256, x), color='green', linewidth=2.0,
-                    linestyle="-.", label='f=0.975')
-line256f3, = ax.plot(x, SpeedupSymmetric(f[3], 256, x), color='purple', linewidth=2.0,
-                    linestyle="--", label='f=0.9')
-line256f4, = ax.plot(x, SpeedupSymmetric(f[4], 256, x), color='blue', linewidth=2.0,
-                    linestyle="-", label='f=0.5')
+line256f0, = ax.plot(x, SpeedupSymmetric(f[0], 256, x), color='red', 
+                    linewidth=2.0,linestyle="-", label='f=0.999')
+line256f1, = ax.plot(x, SpeedupSymmetric(f[1], 256, x), color='coral', 
+                    linewidth=2.0,linestyle=":", label='f=0.99')
+line256f2, = ax.plot(x, SpeedupSymmetric(f[2], 256, x), color='green', 
+                    linewidth=2.0,linestyle="-.", label='f=0.975')
+line256f3, = ax.plot(x, SpeedupSymmetric(f[3], 256, x), color='purple', 
+                    linewidth=2.0,linestyle="--", label='f=0.9')
+line256f4, = ax.plot(x, SpeedupSymmetric(f[4], 256, x), color='blue', 
+                    linewidth=2.0,linestyle="-", label='f=0.5')
 
-ax.legend(loc='upper right')
+ax.legend(loc='upper right', fontsize='small')
 ax.set_xscale('log', basex=2)
 ax.set_xlim(1, 256)
 ax.set_ylim(2, 250)
 ax.set_xticks([1,2,4,8,16,32,64,128,256])
 ax.set_xticklabels(['0','2','4','8','16','32','64','128','256'])
-#ax.annotate('Symmetric, N=16', xy=(1.5, 225))
 
 # rBCEs, the number of total BCEs
 x = np.linspace(1, 16, num=16, endpoint=True)
 #SpeedupAsymmetric N=16
 ax = axs[1,0]
-aline16f0, = ax.plot(x, SpeedupAsymmetric(f[0], 16, x), color='red', linewidth=2.0,
-                    linestyle="-", label='f=0.999')
-aline16f1, = ax.plot(x, SpeedupAsymmetric(f[1], 16, x), color='coral', linewidth=2.0,
-                    linestyle=":", label='f=0.99')
-aline16f2, = ax.plot(x, SpeedupAsymmetric(f[2], 16, x), color='green', linewidth=2.0,
-                    linestyle="-.", label='f=0.975')
-aline16f3, = ax.plot(x, SpeedupAsymmetric(f[3], 16, x), color='purple', linewidth=2.0,
-                    linestyle="--", label='f=0.9')
-aline16f4, = ax.plot(x, SpeedupAsymmetric(f[4], 16, x), color='blue', linewidth=2.0,
-                    linestyle="-", label='f=0.5')
+aline16f0, = ax.plot(x, SpeedupAsymmetric(f[0], 16, x), color='red', 
+                    linewidth=2.0,linestyle="-", label='f=0.999')
+aline16f1, = ax.plot(x, SpeedupAsymmetric(f[1], 16, x), color='coral', 
+                    linewidth=2.0,linestyle=":", label='f=0.99')
+aline16f2, = ax.plot(x, SpeedupAsymmetric(f[2], 16, x), color='green',
+                    linewidth=2.0,linestyle="-.", label='f=0.975')
+aline16f3, = ax.plot(x, SpeedupAsymmetric(f[3], 16, x), color='purple', 
+                    linewidth=2.0,linestyle="--", label='f=0.9')
+aline16f4, = ax.plot(x, SpeedupAsymmetric(f[4], 16, x), color='blue', 
+                    linewidth=2.0,linestyle="-", label='f=0.5')
 
-#ax.legend(loc='upper right')
+#ax.legend(loc='upper right',fontsize='small')
 ax.set_xscale('log', basex=2)
 ax.set_xlim(1, 16)
 ax.set_ylim(2, 16)
 ax.set_xticks([1,2,4,8,16])
 ax.set_xticklabels(['0','2','4','8','16'])
-#ax.annotate('Asymmetric, n=16', xy=(1.5, 15))
 
 # rBCEs, the number of total BCEs
 x = np.linspace(1, 256, num=256, endpoint=True)
 #SpeedupAsymmetric N=256
 ax = axs[1,1]
-aline256f0, = ax.plot(x, SpeedupAsymmetric(f[0], 256, x), color='red', linewidth=2.0,
-                    linestyle="-", label='f=0.999')
-aline256f1, = ax.plot(x, SpeedupAsymmetric(f[1], 256, x), color='coral', linewidth=2.0,
-                    linestyle=":", label='f=0.99')
-aline256f2, = ax.plot(x, SpeedupAsymmetric(f[2], 256, x), color='green', linewidth=2.0,
-                    linestyle="-.", label='f=0.975')
-aline256f3, = ax.plot(x, SpeedupAsymmetric(f[3], 256, x), color='purple', linewidth=2.0,
-                    linestyle="--", label='f=0.9')
-aline256f4, = ax.plot(x, SpeedupAsymmetric(f[4], 256, x), color='blue', linewidth=2.0,
-                    linestyle="-", label='f=0.5')
+aline256f0, = ax.plot(x, SpeedupAsymmetric(f[0], 256, x), color='red', 
+                    linewidth=2.0,linestyle="-", label='f=0.999')
+aline256f1, = ax.plot(x, SpeedupAsymmetric(f[1], 256, x), color='coral', 
+                    linewidth=2.0,linestyle=":", label='f=0.99')
+aline256f2, = ax.plot(x, SpeedupAsymmetric(f[2], 256, x), color='green', 
+                    linewidth=2.0,linestyle="-.", label='f=0.975')
+aline256f3, = ax.plot(x, SpeedupAsymmetric(f[3], 256, x), color='purple', 
+                    linewidth=2.0,linestyle="--", label='f=0.9')
+aline256f4, = ax.plot(x, SpeedupAsymmetric(f[4], 256, x), color='blue', 
+                    linewidth=2.0,linestyle="-", label='f=0.5')
 
-#ax.legend(loc='upper right')
+#ax.legend(loc='upper right',fontsize='small')
 ax.set_xscale('log', basex=2)
 ax.set_xlim(1, 256)
 ax.set_ylim(2, 250)
 ax.set_xticks([1,2,4,8,16,32,64,128,256])
 ax.set_xticklabels(['0','2','4','8','16','32','64','128','256'])
-#ax.annotate('Asymmetric, N=16', xy=(1.5, 225))
 
 # rBCEs, the number of total BCEs
 x = np.linspace(1, 16, num=16, endpoint=True)
 #SpeedupDynamic N=16
 ax = axs[2,0]
-dline16f0, = ax.plot(x, SpeedupDynamic(f[0], 16, x), color='red', linewidth=2.0,
-                    linestyle="-", label='f=0.999')
-dline16f1, = ax.plot(x, SpeedupDynamic(f[1], 16, x), color='coral', linewidth=2.0,
-                    linestyle=":", label='f=0.99')
-dline16f2, = ax.plot(x, SpeedupDynamic(f[2], 16, x), color='green', linewidth=2.0,
-                    linestyle="-.", label='f=0.975')
-dline16f3, = ax.plot(x, SpeedupDynamic(f[3], 16, x), color='purple', linewidth=2.0,
-                    linestyle="--", label='f=0.9')
-dline16f4, = ax.plot(x, SpeedupDynamic(f[4], 16, x), color='blue', linewidth=2.0,
-                    linestyle="-", label='f=0.5')
+dline16f0, = ax.plot(x, SpeedupDynamic(f[0], 16, x), color='red', 
+                    linewidth=2.0,linestyle="-", label='f=0.999')
+dline16f1, = ax.plot(x, SpeedupDynamic(f[1], 16, x), color='coral', 
+                    linewidth=2.0,linestyle=":", label='f=0.99')
+dline16f2, = ax.plot(x, SpeedupDynamic(f[2], 16, x), color='green', 
+                    linewidth=2.0,linestyle="-.", label='f=0.975')
+dline16f3, = ax.plot(x, SpeedupDynamic(f[3], 16, x), color='purple', 
+                    linewidth=2.0,linestyle="--", label='f=0.9')
+dline16f4, = ax.plot(x, SpeedupDynamic(f[4], 16, x), color='blue', 
+                    linewidth=2.0,linestyle="-", label='f=0.5')
 
-#ax.legend(loc='upper right')
+#ax.legend(loc='upper right',fontsize='small')
 ax.set_xscale('log', basex=2)
 ax.set_xlim(1, 16)
 ax.set_ylim(2, 16)
 ax.set_xticks([1,2,4,8,16])
 ax.set_xticklabels(['0','2','4','8','16'])
-#ax.annotate('Dynamic, n=16', xy=(1.5, 15))
 
 # rBCEs, the number of total BCEs
 x = np.linspace(1, 256, num=256, endpoint=True)
 #SpeedupDynamic N=256
 ax = axs[2,1]
-dline256f0, = ax.plot(x, SpeedupDynamic(f[0], 256, x), color='red', linewidth=2.0,
-                    linestyle="-", label='f=0.999')
-dline256f1, = ax.plot(x, SpeedupDynamic(f[1], 256, x), color='coral', linewidth=2.0,
-                    linestyle=":", label='f=0.99')
-dline256f2, = ax.plot(x, SpeedupDynamic(f[2], 256, x), color='green', linewidth=2.0,
-                    linestyle="-.", label='f=0.975')
-dline256f3, = ax.plot(x, SpeedupDynamic(f[3], 256, x), color='purple', linewidth=2.0,
-                    linestyle="--", label='f=0.9')
-dline256f4, = ax.plot(x, SpeedupDynamic(f[4], 256, x), color='blue', linewidth=2.0,
-                    linestyle="-", label='f=0.5')
+dline256f0, = ax.plot(x, SpeedupDynamic(f[0], 256, x), color='red', 
+                    linewidth=2.0,linestyle="-", label='f=0.999')
+dline256f1, = ax.plot(x, SpeedupDynamic(f[1], 256, x), color='coral', 
+                    linewidth=2.0,linestyle=":", label='f=0.99')
+dline256f2, = ax.plot(x, SpeedupDynamic(f[2], 256, x), color='green', 
+                    linewidth=2.0,linestyle="-.", label='f=0.975')
+dline256f3, = ax.plot(x, SpeedupDynamic(f[3], 256, x), color='purple', 
+                    linewidth=2.0,linestyle="--", label='f=0.9')
+dline256f4, = ax.plot(x, SpeedupDynamic(f[4], 256, x), color='blue', 
+                    linewidth=2.0,linestyle="-", label='f=0.5')
 
-#ax.legend(loc='upper right')
+#ax.legend(loc='upper right',fontsize='small')
 ax.set_xscale('log', basex=2)
 ax.set_xlim(1, 256)
 ax.set_ylim(2, 250)
 ax.set_xticks([1,2,4,8,16,32,64,128,256])
 ax.set_xticklabels(['0','2','4','8','16','32','64','128','256'])
-#ax.annotate('Dynamic, N=16', xy=(1.5, 225))
 
 plt.show()
